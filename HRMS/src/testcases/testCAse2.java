d775783e2c539c00191a0e94fbd5fab5ec346d5a
61448 <61448@DESKTOP-RH0FRAC.local>
2023-11-02 19:15:36 +1100

    test case 2 updates

--- HRMS/src/testcases/testCAse2.java
+++ HRMS/src/testcases/testCAse2.java
@@ -4,7 +4,10 @@ public class testCAse2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.print("test case 2");
+     System.out.print("test case 2 ");
+     System.out.print("test case 2 some updates");
+     
+     
 	}
 
 }
